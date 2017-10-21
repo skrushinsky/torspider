373bd1b8fe01a500d4660a94380a990764ff82c9
Sergey Krushinsky <krushinsky@gmail.com>
2017-10-21 15:06:33 +0300

    fixed after testing

--- torspider/urlnorm.py
+++ torspider/urlnorm.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 
 '''
-urlnorm.py - URL normalisation routines
-This is clone of http://github.com/jehiah/urlnorm library adapted to Python 3.
+urlnorm.py - URL utilities.
 '''
-
+import pprint
 from urllib.parse import urlparse, urlunparse, unquote
 import re
-import idna
 import logging
 from pprint import pformat
 
@@ -31,19 +29,45 @@ _server_authority_schemes = ['http', 'https', 'news', 'snews', 'ftp',]
 
 
 def norm(url, domain=None):
+    """
+    Normalize URL.
+
+    Based on http://github.com/jehiah/urlnorm library, adapted to Python 3
+    with some fixes and modifications.
+    Actions:
+
+    1. Converts relative URLs to absolute.
+    2. If protocol is missing, uses 'http'.
+    3. Normalizes cases.
+    4. Removes default port.
+    5. Collapses (optimizes) path.
+    6. Unquotes path fragments.
+    7. Converts international domains from idna to utf8.
+    8. Removes fragment, if any.
+
+    Arguments:
+        url : URL address, a string
+        domain : default domain, used if netloc is missing, i.e. the URL is relative
+
+    Returns:
+        a tuple of values compatiable with urllib.parse.urlparse result.
+
+    Fails with AssertionError if a relative URL can not be converted to absolute.
+    This happens when neither URL contains netloc, nor domain parameter is provided.
+    """
     (scheme, authority, path, parameters, query, fragment) = urlparse(url, scheme='http')
-    if authority:
-        userinfo, host, port = _server_authority.match(authority).groups()
-        if host[-1] == '.':
-            host = host[:-1]
-        authority = host.lower()
-        if userinfo:
-            authority = "%s@%s" % (userinfo, authority)
-        if port and int(port) != _default_port.get(scheme, None):
-            authority = "%s:%s" % (authority, port)
-    else:
-        authority = domain.lower()
+    logging.debug(pprint.pformat((scheme, authority, path, parameters, query, fragment) ))
+    if not authority:
+        authority = domain
     assert authority, '%s: No authority!' % url
+    userinfo, host, port = _server_authority.match(authority).groups()
+    if host[-1] == '.':
+        host = host[:-1]
+    authority = host.lower()
+    if userinfo:
+        authority = "%s@%s" % (userinfo, authority)
+    if port and int(port) != _default_port.get(scheme, None):
+        authority = "%s:%s" % (authority, port)
 
     if scheme in _relative_schemes:
         last_path = path
@@ -55,23 +79,28 @@ def norm(url, domain=None):
 
     path = unquote(path)
     try:
-        authority = idna.decode(authority)
+        authority = authority.encode('utf8').decode('idna')
     except Exception as ex:
         logging.warn(ex)
 
-    parts = (scheme, authority, path, parameters, query, "")
+    parts = (scheme, authority, path, parameters, query, '')
     assert not join_parts(parts).startswith('http://http://'), 'Error parsing <%s> (%s)' % (url, pformat(parts))
     return parts
 
 def join_parts(url):
+    """Convert norm result to string."""
     return urlunparse(url)
 
-
 def first_level_domain(full_domain):
+    """Given a domain name, extract first level domain."""
     return '.'.join(full_domain.split('.')[-2:])
 
 def get_domain(url):
+    """Given an URL, return domain name."""
     return urlparse(url)[1]
 
 def get_first_level_domain(url):
+    """Combination of get_domain and first_level_domain: given URL, return
+    first level domain of its netloc part.
+    """
     return first_level_domain(get_domain(url))
